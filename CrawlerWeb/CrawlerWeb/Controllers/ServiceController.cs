2be57ca083b61d31deab15d0733dc6aba8162fee
mudasirale <mudasir.ale@gmail.com>
2015-11-08 19:14:46 +0500

    Initial web service skeleton

---
+++ CrawlerWeb/CrawlerWeb/Controllers/ServiceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CrawlerWeb.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        [Route("api/getunis")]
+        public string Get([FromUri(Name = "name", SuppressPrefixCheck = false)] string name = "",
+            [FromUri(Name = "sname", SuppressPrefixCheck = false)] string secondName = "")
+        {
+            return "Hi there, " + name + " " + secondName + "!";
+        }
+    }
+}
---
+++ CrawlerWeb/CrawlerWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrawlerWeb.WebApiApplication" Language="C#" %>
